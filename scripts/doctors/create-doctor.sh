6f846303ad1d291f6e0d2391ecb707b28c41662f
beatrizelena <ortizwebdevelopment@gmail.com>
2018-07-24 14:28:24 -0400

    Edit create-doctor.sh curl script to reflect new db schema that no longer includes a column for notes.

--- scripts/doctors/create-doctor.sh
+++ scripts/doctors/create-doctor.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-# TOKEN="BAhJIiUxYTMwZTgxZmUwMzg5OTQzMzc1NzQzNjNjMzc2MmFmZAY6BkVG--31a7ef4194227bda11f173dc3864c33ddd2a03b1" FIRST_NAME='Mary' LAST_NAME='Ortiz' CLINIC_AFFILIATION='JF/Jimmy Fund' SUB_SPECIALTY_ENGLISH='Sarcoma' SUB_SPECIALTY_SPANISH='Sarcoma' PHONE_NUMBER='617-632-3333' sh ./scripts/doctors/create-doctor.sh
+# TOKEN="" FIRST_NAME='Mary' LAST_NAME='Ortiz' CLINIC_AFFILIATION='JF/Jimmy Fund' SUB_SPECIALTY_ENGLISH='Sarcoma' SUB_SPECIALTY_SPANISH='Sarcoma' PHONE_NUMBER='617-632-3333' sh ./scripts/doctors/create-doctor.sh
 # (tokens expire within about 20 minutes of being generated)
 
 curl "http://localhost:4741/doctors" \
