f7748cb67f0e793ff516c6d1f8c5b8637ed199c4
beatrizelena <ortizwebdevelopment@gmail.com>
2018-07-24 09:10:56 -0400

    Edit curl scripts to reflect changes in users table

--- scripts/auth/sign-in.sh
+++ scripts/auth/sign-in.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 
 # EMAIL='b@example.com' PASSWORD='123' sh ./scripts/auth/sign-in.sh
+# EMAIL='beatriz@example.com' PASSWORD='123' sh ./scripts/auth/sign-in.sh
+# EMAIL='beatriz.ortiz.2018@example.com' PASSWORD='123' sh ./scripts/auth/sign-in.sh
 
 curl "http://localhost:4741/sign-in" \
   --include \
--- scripts/auth/sign-up.sh
+++ scripts/auth/sign-up.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
-EMAIL='beatriz.ortiz.2018@example.com' PASSWORD='123' PASSWORD_CONFIRMATION='123' sh ./scripts/auth/sign-up.sh
+# EMAIL='beatriz.ortiz.2018@example.com' PASSWORD='123' PASSWORD_CONFIRMATION='123' FIRST_NAME='Bea' LAST_NAME='Ortiz' sh scripts/auth/sign-up.sh
+# EMAIL='beatriz.ortiz.2019@example.com' PASSWORD='123' PASSWORD_CONFIRMATION='123' FIRST_NAME='Bea' LAST_NAME='Ortiz' sh scripts/auth/sign-up.sh
 
 curl "http://localhost:4741/sign-up" \
   --include \
@@ -10,7 +11,9 @@ curl "http://localhost:4741/sign-up" \
     "credentials": {
       "email": "'"${EMAIL}"'",
       "password": "'"${PASSWORD}"'",
-      "password_confirmation": "'"${PASSWORD}"'"
+      "password_confirmation": "'"${PASSWORD_CONFIRMATION}"'",
+      "first_name": "'"${FIRST_NAME}"'",
+      "last_name": "'"${LAST_NAME}"'"
     }
   }'
 
