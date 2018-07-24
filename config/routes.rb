efa8a3f7f4944dedd498229994ad2beed54e6334
beatrizelena <ortizwebdevelopment@gmail.com>
2018-07-24 16:07:41 -0400

    Minor edit to routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,8 @@
 # frozen_string_literal: true
 
 Rails.application.routes.draw do
-  resources :sessions
   # RESTful routes
+  resources :sessions
   resources :doctors
   resources :users, only: %i[index show update]
 
