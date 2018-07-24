7e898609ef3226b29e45c1c76625715fd3ba6f4e
beatrizelena <ortizwebdevelopment@gmail.com>
2018-07-24 09:21:46 -0400

    Add routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,8 @@
 # frozen_string_literal: true
 
 Rails.application.routes.draw do
+  resources :sessions
   # RESTful routes
-  resources :doctorprofiles, except: %i[new edit]
   resources :doctors, except: %i[new edit]
   resources :users, only: %i[index show update]
 
