a469b84ae7850ec019c08cbcbbd18e9de84b4abb
beatrizelena <ortizwebdevelopment@gmail.com>
2018-07-24 16:03:13 -0400

    Add directory and files to run curl scripts for sessions table.

---
+++ scripts/sessions/sessions-create.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+# NOTE="Hello, doc" USERID=3 DOCTORID=2 sh scripts/sessions/sessions-create.sh
+
+curl --include --request POST "http://localhost:4741/sessions" \
+  --header "Content-Type: application/json" \
+  --data '{
+    "session": {
+      "user_id": "'"${USERID}"'",
+      "doctor_id": "'"${DOCTORID}"'",
+      "note": "'"${NOTE}"'"
+    }
+  }'
