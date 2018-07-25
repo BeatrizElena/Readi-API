984307530adb29185598bd386f3a498a7ab6a5e7
beatrizelena <ortizwebdevelopment@gmail.com>
2018-07-25 15:30:52 -0400

    Minor edit to commented out command for running session-create.sh file

--- scripts/sessions/sessions-create.sh
+++ scripts/sessions/sessions-create.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 
-# NOTES="Hello, doc" USERID=3 DOCTORID=2 sh scripts/sessions/sessions-create.sh
+# NOTES="Hello, doc" TOKEN="BAhJIiViMjVkYTQ0M2UyZGE2YmU4ZDIwMzViZTFkY2NmNTIzZAY6BkVG--bd05617d596c2e63f3b84e2f0a9c8414476743bb" DOCTORID=2 sh scripts/sessions/sessions-create.sh
 
 curl --include --request POST "http://localhost:4741/sessions" \
   --header "Content-Type: application/json" \
+  --header "Authorization: Token token=${TOKEN}" \
   --data '{
     "session": {
-      "user_id": "'"${USERID}"'",
       "doctor_id": "'"${DOCTORID}"'",
       "notes": "'"${NOTES}"'"
     }
